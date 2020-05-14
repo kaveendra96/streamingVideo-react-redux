a3c27a8a324728e4eaaede8da4113a885d90805c
nipun <kaveendra96@gmail.com>
2020-05-14 09:54:38 +0530

    google-Oauth

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,11 +1,13 @@
+import { SIGN_OUT ,SIGN_IN } from '../action/types'
+
 const INTIAL_STATE ={
     isSignedIn:null
 }
 export default (state =INTIAL_STATE ,action) =>{
     switch(action.type){
-        case 'SIGN_IN':
+        case SIGN_IN:
             return { ...state ,isSignedIn:true}
-        case 'SIGN_OUT':
+        case SIGN_OUT:
             return { ...state ,isSignedIn:false}
         default:
             return state
